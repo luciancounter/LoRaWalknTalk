838659314aec787572b0f48ef1ea28c611fe96d7
luciancounter <113475636+luciancounter@users.noreply.github.com>
2022-10-08 17:35:40 -0500

    Add files via upload

---
+++ rover_keywip.ino
@@ -0,0 +1,219 @@
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
+
+// Mario's Ideas
+// Morse Code transmiter
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Oled display size
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_SDA 21
+#define OLED_SCL 22
+#define OLED_RST 16
+
+//Potentiometer PIN A1
+  
+
+// Variables capturing current and newly calculated position on the letter board (Values 0-26)
+  int New_Position=0;
+  int Old_Position=0;
+
+// Variable capturing Potentimeter read (Values 0 1023)
+  int Pot_read=0;
+
+// Variable used for calculating moving average of potentiometer reading to stabilise the input
+  int Count_For_Moving_Average=0;
+
+// String variable holding the text to transmit
+  String To_Transmit="";
+
+// Length of the text to transmit
+  int To_Transmit_Length=0;
+
+// Flag indicating the letter was entered on the leter board to be added to To_Transmit string
+  int Letter_Entered=0;
+  int Transmit=0;
+  int But = 0;
+  int lft = 0;
+
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+
+// Used for displaying Leter board
+char Letters[28]="ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+
+
+void Add_To_String(){
+
+  Letter_Entered=1;
+  
+}
+
+void ButtonLeft(){
+  lft = 1;
+}
+
+void ButtonRight(){
+  But = 1;
+}
+
+void Triger_Transmit(){
+  
+  Transmit=1;
+
+}
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void setup() {
+  Serial.begin(9600);
+  // Define two interrupts
+  // D2- when button pressed the current letter on the letter board is entered to To_Transmit
+  // D3- When button is pressed To_Transmit string is output in Morse code 
+  attachInterrupt(32,Add_To_String, FALLING);
+  attachInterrupt(33,Triger_Transmit, FALLING);
+  attachInterrupt(35,ButtonRight, FALLING);
+  attachInterrupt(15,ButtonLeft, FALLING);
+
+Wire.begin(OLED_SDA, OLED_SCL);
+  
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C, false, false)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+    
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+  
+  // Clear the buffer
+  display.clearDisplay();
+  display.display();
+  
+  // Display Letter Board 3 rows 9 character in each row 
+  display.setTextSize(2);            
+  display.setTextColor(SSD1306_WHITE);       
+  for (int j=0; j<3;j++){
+  for (int i=0; i<9;i++){
+     display.setCursor(i*12+2*i+1,j*16+17);           
+     display.println(Letters[i+j*9]);
+     //display.fillRect(i*12+2*i, j*16 +16, 12, 16, SSD1306_INVERSE);
+     display.display();
+  }
+  }
+  // Display filled in rect in the top section of the display when To_Transfer would be output
+  display.fillRect(0, 0, 128, 15, SSD1306_INVERSE);
+  // Highlight character A by displaying Inverse rect at first position
+  display.fillRect(0, 16, 12, 16, SSD1306_INVERSE);
+
+  display.display();
+}
+
+void Highlight_letter(int New_Pos, int Old_Pos){
+  // When position changes from Old_Pos to New_Pos
+  // Draw the inverse rect in the Old_pos to deactivate  the highlight in the old spot
+  // Draw the inverse rect to Highlite the new spot
+  int X_pos;
+  int Y_pos;
+  
+  // Calculate X and Y coordinates of the New_Pos on the letter board
+  X_pos=New_Pos - ((int)New_Pos/9)*9;
+  Y_pos=(int)New_Pos/9;
+  // Displaying Inverse rect
+  display.fillRect(X_pos*12+2*X_pos, Y_pos*16 +16, 12, 16, SSD1306_INVERSE);
+  
+  // Calculate X and Y coordinates of the Old_Pos on the letter board
+  X_pos=Old_Pos - ((int)Old_Pos/9)*9;
+  Y_pos=(int)Old_Pos/9;
+  // Displaying Inverse rect
+  display.fillRect(X_pos*12+2*X_pos, Y_pos*16 +16, 12, 16, SSD1306_INVERSE);
+  
+  display.display();
+}
+
+void loop() 
+{
+  if(lft == 1){
+    New_Position--;
+
+    if (Old_Position!=New_Position){
+       Highlight_letter (New_Position,Old_Position);
+       Old_Position=New_Position;
+    }
+    lft =0;
+  }
+  // Increase Count_For_Moving_Average 30 times before calcluating moving average
+  //Count_For_Moving_Average++;
+
+  if(But == 1){
+      New_Position++;
+      But = 0;
+
+      if (Old_Position!=New_Position){
+       Highlight_letter (New_Position,Old_Position);
+       Old_Position=New_Position;
+    }
+  }
+    
+    // Calculate moving average for 30 potentiometer reads
+    // Without calculating moving average reads are to volatile
+    //Pot_read=(int)Pot_read/30;
+
+    // As potentiometer read was not stable around 0 and 1023 values
+    // Here is the code to fix it and make sure that values 0 and 26 are properly mapped
+    // Between values 101 and 899 the potentiometer read is mapped to the range 0,26
+    // New_Position is the new position calculated on the letter board corresponding to 
+    //potentiometer read
+    /*
+    if (Pot_read<0 or Pot_read==0) New_Position=0;
+    if (Pot_read >0 or Pot_read<4095) New_Position++;
+    if (Pot_read>4095 or Pot_read==4095) New_Position=26;
+    */
+    // If new position differes fro the previous one
+    // remove highlight from the old one and Highlight a new one
+    // New position becomes also the current position now
+    
+
+    // reset Count_For_Moving_Average and Pot_read to start calculating the new moving average
+    
+  
+
+  // Wait 100 loop cycles before checking if the new letter was entered
+  
+  // If letter was entered add the letter to To_Transmit and output it on the display
+  if (Letter_Entered==1 ){
+      To_Transmit=To_Transmit + Letters[New_Position];
+      To_Transmit_Length++;
+      display.setCursor(3,0);
+      display.setTextColor(BLACK     );
+      display.fillRect(0, 0, 128, 15, SSD1306_WHITE);
+      display.println(To_Transmit);
+      display.display();
+      Letter_Entered=0;
+      delay(100);
+  }
+  // If Transmit button was pressed start morse code transmition
+  
+  if (Transmit==1){
+    Serial.print("\n" + To_Transmit);
+    To_Transmit="";
+    display.setTextColor(BLACK);
+    display.fillRect(0,0,128,15,SSD1306_WHITE);\
+    display.println(To_Transmit);
+    display.display();
+    Transmit=0;
+    
+  }
+  
+}
